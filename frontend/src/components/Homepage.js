076cfb04bbb03a3ca3e4c26e3b7ed42136c4c7db
ChrisnellJoy19 <cjlimpiada03@gmail.com>
2025-03-03 22:57:03 +0800

    Fixed frontend

---
+++ frontend/src/components/Homepage.js
@@ -0,0 +1,43 @@
+import React from "react";
+import "../styles/Homepage.css"; // Import the CSS file
+
+const Homepage = () => {
+  return (
+    <div className="homepage">
+      {/* Top Bar */}
+      <div className="rectangle-2"></div>
+
+      {/* Navigation */}
+      <nav className="navigation">
+      <div className="logo-container">
+        <img src="/images/dilg_logo.png" alt="DILG Logo" className="dilg-logo" />
+        <span className="site-title">DILG Marinduque</span>
+      </div>  
+        <div className="nav-items">
+          <a href="#" className="nav-item">Login</a>
+          <a href="#" className="nav-item">Sign Up</a>
+          <a href="#" className="nav-item">About Us</a>
+        </div>
+      </nav>
+
+      {/* Title and Description */}
+      <div className="description">
+        {/* <h1>Welcome to BLTS</h1> */}
+        <p>Barangay Legislative Tracking System (BLTS) is an online repository platform for archiving Barangay Legislative Records. Barangay Secretary uploads ordinances, resolutions and others.</p>
+      </div>
+
+      {/* Get Started Button */}
+      <a href="#" className="get-started-btn">Get Started</a>
+
+      {/* BLTS Logo */}
+      <img src="/images/blts_logo.png" alt="BLTS Logo" className="blts-logo" />
+
+      {/* Footer */}
+      <footer className="footer">
+        <p className="footer-text">A project by ONE MARINDUQUE DILG - LRC</p>
+      </footer>
+    </div>
+  );
+};
+
+export default Homepage;
