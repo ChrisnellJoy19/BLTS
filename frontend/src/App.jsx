389e916ab9b877cdf6394ecedc027e52d00216eb
lykalyks01 <jelaicamaemayores@gmail.com>
2025-03-10 10:40:11 +0800

    tailwind css

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -4,18 +4,21 @@ import Homepage from "./components/Homepage";
 import Login from "./components/Login";
 import Sign_Up from "./components/Sign_Up";
 import About_Us from "./components/About_Us";
+import './index.css'; // Make sure Tailwind is imported
 
 function App() {
   return (
-    <Router>
-      <Routes>
-        <Route path="/" element={<Homepage />} />
-        <Route path="/login" element={<Login />} />
-        <Route path="/homepage" element={<Homepage />} />
-        <Route path="/signup" element={<Sign_Up />} /> 
-        <Route path="/about" element={<About_Us />} /> 
-      </Routes>
-    </Router>
+    <div className="min-h-screen bg-gray-100 text-gray-900">
+      <Router>
+        <Routes>
+          <Route path="/" element={<Homepage />} />
+          <Route path="/login" element={<Login />} />
+          <Route path="/homepage" element={<Homepage />} />
+          <Route path="/signup" element={<Sign_Up />} /> 
+          <Route path="/about" element={<About_Us />} /> 
+        </Routes>
+      </Router>
+    </div>
   );
 }
 
