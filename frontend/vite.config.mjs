6cb64bd551216646f68579f454a8f0575d597f45
lykalyks01 <jelaicamaemayores@gmail.com>
2025-04-28 02:09:27 +0800

    updated vercel.json

--- frontend/vite.config.mjs
+++ frontend/vite.config.mjs
@@ -13,7 +13,10 @@ export default defineConfig({
       '@': path.resolve(__dirname, './src'),
     },
   },
-  build:{
-    outDir: 'dist',
+  build: {
+    outDir: 'dist', // Ensures that the output goes into the 'dist' folder
+    rollupOptions: {
+      input: path.resolve(__dirname, 'frontend/index.html'), // Explicitly point to the index.html file
+    },
   },
 });
